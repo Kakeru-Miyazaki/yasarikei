fd7ffe261fc91843a068965868db7a43556b4bfc
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-12-15 00:15:06 +0900

    ok

--- ekichika/multi_person.js
+++ ekichika/multi_person.js
@@ -13,8 +13,6 @@ function multi_person() {
   }
 }
 
-clicked_stations_names;
-
 function show_center_station() {
   meet_up(clicked_stations, 180, stationQueue, goal_station_name_ID).then(
     () => {
@@ -26,69 +24,119 @@ function show_center_station() {
           .ease(d3.easeLinear)
           .attr("opacity", 1);
       }
-      g.selectAll(".circle").attr("fill-opacity", 0.2).attr("fill", "white");
-      g.selectAll(".wave_in").remove();
-      for (var clicked_station_id of clicked_stations_names) {
-        g.select("#" + clicked_station_id)
-          .attr("fill-opacity", 1)
-          .attr("fill", "red");
-      }
-      if (goal_station_name_ID.length == 1) {
+      if (goal_station_name_ID.length > 0) {
         // center_stationは駅名+グループID
+        console.log(goal_station_name_ID);
         var center_station = goal_station_name_ID.shift();
+
+        if (last_center_station != NaN) {
+          if (last_center_station_red_flag == 1) {
+            g.select("#" + last_center_station)
+              .attr("fill-opacity", 1)
+              .attr("fill", "red");
+          } else {
+            g.select("#" + last_center_station)
+              .transition()
+              .duration(500)
+              .ease(d3.easeLinear)
+              .attr("fill-opacity", 0.2)
+              .attr("fill", "white");
+          }
+          g.select(".wave_in").remove();
+        }
+        console.log(g.select("#" + center_station).attr("fill"));
+        console.log(last_nearest_station_red_flag);
+        if (
+          g.select("#" + center_station).attr("fill") == "rgb(255, 0, 0)" ||
+          g.select("#" + center_station).attr("fill") == "red" ||
+          (g.select("#" + center_station).attr("fill") == "rgb(148, 0, 211)" &&
+            last_nearest_station_red_flag == 1)
+        ) {
+          console.log("conter_station_red !!");
+          last_center_station_red_flag = 1;
+        } else {
+          console.log("conter_station_not_red !!");
+          last_center_station_red_flag = 0;
+        }
         g.select("#" + center_station)
           .transition()
           .duration(500)
           .ease(d3.easeLinear)
           .attr("fill-opacity", 1)
           .attr("fill", "lime");
-        show_waves(center_station);
-      } else if (goal_station_name_ID.length == 2) {
-        var center_station = goal_station_name_ID.shift();
-        var nearest_hub_station = goal_station_name_ID.shift();
-        g.select("#" + center_station)
-          .transition()
-          .duration(500)
-          .ease(d3.easeLinear)
-          .attr("fill-opacity", 1)
-          .attr("fill", "lime");
-        g.select("#" + nearest_hub_station)
-          .transition()
-          .duration(500)
-          .ease(d3.easeLinear)
-          .attr("fill-opacity", 1)
-          .attr("fill", "darkviolet");
-        show_waves(center_station);
-        show_waves(nearest_hub_station);
+        last_center_station = center_station;
+        var circle_cx = d3.select("#" + center_station).attr("cx");
+        var circle_cy = d3.select("#" + center_station).attr("cy");
+        var circle_transform = d3
+          .select("#" + center_station)
+          .attr("transform");
+        var kakudairitu =
+          0.2 / d3.select("#" + center_station).attr("stroke-width");
+        g.selectAll(".wave_out_animation").attr("to", 8);
+
+        var wave = g
+          .append("circle")
+          .attr("stroke", "white")
+          .attr("stroke-width", 0.2)
+          .attr("class", "wave wave_in")
+          .attr("opacity", 1)
+          .attr("fill-opacity", 0)
+          .attr("cx", circle_cx)
+          .attr("cy", circle_cy)
+          .attr("r", 0)
+          .attr("transform", circle_transform);
+
+        wave
+          .append("animate")
+          .attr("attributeName", "r")
+          .attr("begin", "0s")
+          .attr("dur", "1s")
+          .attr("from", "25")
+          .attr("to", "0")
+          .attr("repeatCount", "indefinite");
+
+        if (last_nearest_station != NaN) {
+          if (last_nearest_station_red_flag == 1) {
+            g.select("#" + last_nearest_station)
+              .attr("fill-opacity", 1)
+              .attr("fill", "red");
+          } else {
+            g.select("#" + last_nearest_station)
+              .transition()
+              .duration(500)
+              .ease(d3.easeLinear)
+              .attr("fill-opacity", 0.2)
+              .attr("fill", "white");
+          }
+        }
+
+        //ハブ駅の表示
+        if (goal_station_name_ID.length > 0) {
+          var nearest_hub_station = goal_station_name_ID.shift();
+
+          console.log(g.select("#" + nearest_hub_station).attr("fill"));
+          if (
+            g.select("#" + nearest_hub_station).attr("fill") ==
+              "rgb(255, 0, 0)" ||
+            g.select("#" + nearest_hub_station).attr("fill") == "red" ||
+            (g.select("#" + center_station).attr("fill") == "rgb(0, 255, 0)" &&
+              last_center_station_red_flag == 1)
+          ) {
+            console.log("hub_station_red !!");
+            last_nearest_station_red_flag = 1;
+          } else {
+            console.log("hub_station_not_red !!");
+            last_nearest_station_red_flag = 0;
+          }
+          g.select("#" + nearest_hub_station)
+            .transition()
+            .duration(500)
+            .ease(d3.easeLinear)
+            .attr("fill-opacity", 1)
+            .attr("fill", "darkviolet");
+          last_nearest_station = nearest_hub_station;
+        }
       }
     }
   );
-}
-
-function show_waves(ekiID) {
-  var circle_cx = d3.select("#" + ekiID).attr("cx");
-  var circle_cy = d3.select("#" + ekiID).attr("cy");
-  var circle_transform = d3.select("#" + ekiID).attr("transform");
-  g.selectAll(".wave_out_animation").attr("to", 8);
-
-  var wave = g
-    .append("circle")
-    .attr("stroke", "white")
-    .attr("stroke-width", 0.2)
-    .attr("class", "wave wave_in")
-    .attr("opacity", 1)
-    .attr("fill-opacity", 0)
-    .attr("cx", circle_cx)
-    .attr("cy", circle_cy)
-    .attr("r", 0)
-    .attr("transform", circle_transform);
-
-  wave
-    .append("animate")
-    .attr("attributeName", "r")
-    .attr("begin", "0s")
-    .attr("dur", "1s")
-    .attr("from", "25")
-    .attr("to", "0")
-    .attr("repeatCount", "indefinite");
 }
